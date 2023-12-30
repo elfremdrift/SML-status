eb3c704eeac18b587ed496578aed0b71de46ab6e
S. J. Hansen <git@sjh.dk>
2023-12-30 18:24:16 +0100

    Parser now parses correctly and calculates sum

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -3,39 +3,80 @@
 #include <cstring>
 #include <iostream>
 #include <iomanip>
-#include "parser.hpp"
 
-#define DBG(level) std::cout << std::setw(level*2) << ""
+#define DBG_LEVEL 2
+
+#if DBG_LEVEL >= 3
+#define DBG_TRACE(stuff) std::cout << std::dec << std::setw((level-stack)*2) << "" << stuff << "\n";
+#else
+#define DBG_TRACE(stuff)
+#endif
+
+#if DBG_LEVEL >= 2
+#define DBG_INFO_BEGIN std::cout << std::dec << std::setw((level-stack)*2) << "";
+#define DBG_INFO_STUFF(stuff) std::cout << stuff;
+#define DBG_INFO_END std::cout << "\n";
+#else
+#define DBG_INFO_BEGIN()
+#define DBG_INFO_STUFF(stuff)
+#define DBG_INFO_END()
+#endif
+#define DBG_INFO(stuff) DBG_INFO_BEGIN DBG_INFO_STUFF(stuff) DBG_INFO_END
+
+#if DBG_LEVEL >= 1
+#define DBG_ERROR(stuff) std::cout << std::dec << std::setw((level-stack)*2) << "" << stuff << "\n";
+#else
+#define DBG_ERROR(stuff)
+#endif
+
+#include "parser.hpp"
 
 void SMLParser::reset()
 {
-	std::cerr << "Calling reset at level " << int{level} << "\n";
-	isInside = false;
-	level = 0;
+	DBG_ERROR("Calling reset at level " << getLevel())
+	toOutside();
+}
+
+#if DBG_LEVEL > 0
+int SMLParser::getLevel() const
+{
+	return level-stack;
+}
+#endif
+
+void SMLParser::toOutside()
+{
+	state = State::outside;
 	n1b = 0;
 	n01 = 0;
+	actSum = 0;
 }
 
 void SMLParser::outside(const byte ch)
 {
 	switch (ch) {
 	case 0x1b:
-		if (n1b < 4) ++n1b;
+		if (n1b < 4) {
+			crc16(ch);
+			++n1b;
+		}
 		break;
 
 	case 0x01:
 		if (n1b == 4) {
+			crc16(ch);
 			++n01;
-			if (n01 == 4) isInside = true;
+			if (n01 == 4) {
+				toInside();
+			}
 		}
 		else {
-			n1b = 0;
+			toOutside();
 		}
 		break;
 
 	default:
-		n1b = 0;
-		n01 = 0;
+		toOutside();
 		break;
 	}
 }
@@ -43,180 +84,239 @@ void SMLParser::outside(const byte ch)
 void SMLParser::enter()
 {
 	++level;
-	if (level >= stack.size()) {
-		std::cerr << "Stack level too high (" << int{level} << ")\n";
+	if (level-stack >= SML_STACK_SIZE) {
+		std::cerr << "Stack level too high (" << level-stack << ")\n";
 		return reset();
 	}
-	stack[level].init();
+	level->init();
 }
 
 void SMLParser::leave()
 {
-	if (level == 0) {
-		stack[level].init();
+	if (level == stack) {
+		level->init();
 	}
-	else if (level > 0) {
+	else if (level > stack) {
 		--level;
-		while (stack[level].type == Type::array && stack[level].current >= stack[level].length) {
-			if (level > 0) {
+		while (level->type == Type::array && level->current >= level->length) {
+			if (level > stack) {
 				--level;
 			}
 			else {
-				stack[level].init();
+				level->init();
 			}
 		}
 	}
-	DBG(level) << "Leave - now at level " << int{level} << "\n";
+	DBG_TRACE("Leave - now at level " << getLevel())
+}
+
+void SMLParser::toInside()
+{
+	level = stack;
+	level->init();
+	if (state != State::inside) {
+		DBG_INFO("Message begins");
+	}
+	state = State::inside;
 }
 
 void SMLParser::inside(const byte ch)
 {
-	// DBG(level) << "Byte at level " << int{level} << ": " << std::hex << int{ch} << "\n";
-	auto& entry = stack[level];
-	if (entry.lengthFollows) {
-		entry.length = (entry.length << 4) | (ch & 0x0f);
-		entry.lengthFollows = (ch & 0x80) != 0;
-		++entry.protoBytes;
-		if (!entry.lengthFollows && entry.type != Type::array) entry.length -= entry.protoBytes;  // As type and length bytes are counted as well
-		if (entry.lengthFollows) return;
-	}
-	switch (entry.type) {
-	case Type::tbd:
-		entry.tbd(*this, ch);
-		break;
+	crc16(ch);
+	DBG_TRACE("Byte at level " << getLevel() << ": " << std::hex << int{ch})
+	bool repeat = true;
+	while (repeat) {
+		if (lengthFollows) {
+			level->length = (level->length << 4) | (ch & 0x0f);
+			lengthFollows = (ch & 0x80) != 0;
+			++protoBytes;
+			if (!lengthFollows && level->type != Type::array) level->length -= protoBytes;  // As type and length bytes are counted as well
+			if (!lengthFollows) {
+				DBG_TRACE("Entry type " << static_cast<int>(level->type) << " size " << level->length << " encountered at level " << getLevel())
+			}
+			return;
+		}
+		switch (level->type) {
+		case Type::tbd:
+			if (ch == 0x1b) {
+				++n1b;
+				if (n1b == 4) {
+					return toSum();
+				}
+				return;
+			}
+			else {
+				n1b = 0;
+			}
+			repeat = tbd(ch);
+			break;
 
-	case Type::array:
-		entry.array(*this, ch);
-		break;
+		case Type::array:
+			repeat = array(ch);
+			break;
 
-	case Type::boolean:
-		entry.boolean(*this, ch);
-		break;
+		case Type::boolean:
+			repeat = boolean(ch);
+			break;
 
-	case Type::integer:
-		entry.integer(*this, ch, true);
-		break;
+		case Type::integer:
+			repeat = integer(ch, true);
+			break;
 
-	case Type::uinteger:
-		entry.integer(*this, ch, false);
-		break;
-		
-	case Type::string:
-		entry.string(*this, ch);
-		break;
+		case Type::uinteger:
+			repeat = integer(ch, false);
+			break;
+			
+		case Type::string:
+			repeat = string(ch);
+			break;
+		}
 	}
 }
 
-void SMLParser::StackEntry::done(SMLParser& parser)
+void SMLParser::done()
 {
-	switch (type) {
+	switch (level->type) {
 	case Type::integer:
-		DBG(parser.level) << "Got signed integer length " << int{length} << ": " << value.integer << "\n";
+		DBG_INFO("Got signed integer length " << int{level->length} << ": " << value.integer <<
+			"/0x" << std::hex << value.integer)
 		break;
 
 	case Type::uinteger:
-		DBG(parser.level) << "Got unsigned integer length " << int{length} << ": " << value.uinteger << "\n";
+		DBG_INFO("Got unsigned integer length " << int{level->length} << ": " << value.uinteger <<
+			"/0x" << std::hex << value.uinteger)
 		break;
 		
 	case Type::string:
-		DBG(parser.level) << "Got string length " << int{length} << ": ";
-		for (byte i = 0; i < length && i < sizeof(value.string); ++i) {
-			std::cout << std::hex << int{value.string[i]} << " ";
+		DBG_INFO_BEGIN
+		DBG_INFO_STUFF("Got string length " << level->length << ": " << std::hex)
+		for (byte i = 0; i < level->length && i < sizeof(value.string); ++i) {
+			DBG_INFO_STUFF(int{value.string[i]} << " ")
 		}
-		std::cout << "\n";
+		DBG_INFO_END
 		break;
 
 	default:
 		break;
 	}
-	parser.leave();
+	leave();
 }
 
-void SMLParser::StackEntry::tbd(SMLParser& parser, const byte ch)
+bool SMLParser::tbd(const byte ch)
 {
 	// Look at ch to determine what this entry is
 	if (ch == 0) {
-		DBG(parser.level) << "Eol encountered at level " << int{parser.level} << "\n";
-		return done(parser);
+		DBG_INFO("Eol encountered at level " << getLevel())
+		done();
+		return false;
 	}
 
 	auto entType = static_cast<Type>((ch & 0x70) >> 4);
+	switch (entType) {
+	case Type::string:
+		memset(value.string, 0, sizeof(SML_STRING_MAX_LEN));
+		break;
+	case Type::boolean:
+		value.boolean = false;
+		break;
+	case Type::integer:
+		value.integer = 0;
+		break;
+	case Type::uinteger:
+		value.uinteger = 0;
+		break;
+	default:
+		break;
+	}
 	switch (entType) {
 	case Type::string:
 	case Type::boolean:
 	case Type::integer:
 	case Type::uinteger:
 	case Type::array:
-		type = entType;
-		length = ch & 0x0f;
+		level->type = entType;
+		level->length = ch & 0x0f;
 		lengthFollows = (ch & 0x80) != 0;
 		protoBytes = 1;
-		if (!lengthFollows && type != Type::array) length -= protoBytes;  // As type and length bytes are counted as well
-		if (length == 0) {
-			// Already done
-			done(parser);
+		if (!lengthFollows && level->type != Type::array) level->length -= protoBytes;  // As type and length bytes are counted as well
+		if (!lengthFollows) {
+			DBG_TRACE("Entry type " << static_cast<int>(level->type) << " size " << level->length << " encountered at level " << getLevel())
 		}
-		DBG(parser.level) << "Entry type " << static_cast<int>(type) << " encountered at level " << int{parser.level} << "\n";
+		if (level->length == 0) {
+			// Already done
+			done();
+		}
 		break;
 
 	default:
-		std::cerr << "Unknown entry type byte (" << std::hex << int{ch} << ")\n";
-		return parser.reset();
+		DBG_ERROR("Unknown entry type byte (" << std::hex << int{ch} << ")");
+		reset();
 	}
+	return false;
 }
 
-void SMLParser::StackEntry::array(SMLParser& parser, const byte ch)
+bool SMLParser::array(const byte ch)
 {
-	DBG(parser.level) << "Parse array row " << int{current} << " at level " << int{parser.level} << "\n";
-	if (current < length) {
-		++current;
-		parser.enter();  // Read next array entry
-		parser.parse(ch);  // Recurse to parse ch at new level
+	if (level->current == 0) {
+		DBG_INFO("Array size " << level->length << " encountered at level " << getLevel())
 	}
+	DBG_TRACE("Parse array row " << level->current << " at level " << getLevel())
+	if (level->current < level->length) {
+		++level->current;
+		enter();  // Read next array entry
+		return true;  // Re-use char on next level
+	}
+	return false;
 }
 
 void SMLParser::StackEntry::init()
 {
-	*this = StackEntry{};
+	type = Type::tbd;
+	length = 0;
+	current = 0;
 }
 
-void SMLParser::StackEntry::boolean(SMLParser& parser, const byte ch)
+bool SMLParser::boolean(const byte ch)
 {
-	DBG(parser.level) << "Parse boolean byte " << int{current} << " at level " << int{parser.level} << "\n";
-	if (current < length) {
-		++current;
+	DBG_TRACE("Parse boolean byte " << level->current << " at level " << getLevel())
+	if (level->current < level->length) {
+		++level->current;
 	}
-	if (current >= length) {
-		done(parser);
+	if (level->current >= level->length) {
+		done();
 	}
+	return false;
 }
 
-void SMLParser::StackEntry::string(SMLParser& parser, const byte ch)
+bool SMLParser::string(const byte ch)
 {
-	// DBG(parser.level) << "Parse string byte " << int{current} << " at level " << int{parser.level} << "\n";
-	if (current < length) {
-		if (current < sizeof(value.string)) value.string[current] = ch;
-		++current;
+	DBG_TRACE("Parse string byte " << level->current << " at level " << getLevel())
+	if (level->current < level->length) {
+		if (level->current < sizeof(value.string)) value.string[level->current] = ch;
+		++level->current;
 	}
-	if (current >= length) {
-		done(parser);
+	if (level->current >= level->length) {
+		done();
 	}
+	return false;
 }
 
-void SMLParser::StackEntry::integer(SMLParser& parser, const byte ch, const bool hasSign)
+bool SMLParser::integer(const byte ch, const bool hasSign)
 {
-	// DBG(parser.level) << "Parse integer byte " << int{current} << " at level " << int{parser.level} << "\n";
+	DBG_TRACE("Parse integer byte " << level->current << " at level " << getLevel())
 
-	if (current == 0) {
-		value.uinteger = 0;
+	byte* val = hasSign?reinterpret_cast<byte*>(&value.uinteger):reinterpret_cast<byte*>(&value.integer);
+
+	if (level->current == 0) {
+		*reinterpret_cast<uint64_t*>(val) = 0;
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-		next = &value.string[sizeof(uint64_t)-1];
+		next = val + sizeof(uint64_t) - 1;
 #endif
 #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-		next = &value.string;
+		next = val;
 #endif
-		if (sizeof(uint64_t) > length) {
-			for (byte i = sizeof(uint64_t)-length; i>0; --i) {
+		if (level->length < sizeof(uint64_t)) {
+			for (byte i = sizeof(uint64_t)-level->length; i>0; --i) {
 				if (hasSign && (ch & 0x80)) {
 					*next = 0xff;
 				}
@@ -231,8 +331,8 @@ void SMLParser::StackEntry::integer(SMLParser& parser, const byte ch, const bool
 	}
 
 
-	if (current < length) {
-		if (current < sizeof(uint64_t)) {
+	if (level->current < level->length) {
+		if (level->current < sizeof(uint64_t)) {
 			*next = ch;
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 			--next;
@@ -241,23 +341,67 @@ void SMLParser::StackEntry::integer(SMLParser& parser, const byte ch, const bool
 			++next;
 #endif
 		}
-		++current;
+		++level->current;
 	}
-	// DBG(parser.level) << std::hex << "So far: " << value.uinteger << "\n";
+	DBG_TRACE("Int so far: " << std::hex << value.uinteger)
 
-	if (current >= length) {
-		done(parser);
+	if (level->current >= level->length) {
+		done();
 	}
+	return false;
+}
+
+void SMLParser::toSum()
+{
+	state = State::sum;
+	expSum = 0;
+	n01 = 0;
+}
+
+void SMLParser::sum(const byte ch)
+{
+	if (n01 == 0 && ch != 0x1a) {
+		// Not a sum, assume outside and start parsing again
+		toOutside();
+		DBG_INFO("Message complete");
+		parse(ch);
+	}
+	if (n01 < 2) crc16(ch);
+	if (n01 >= 2) {
+		expSum |= ch << (8 * (n01 & 1));
+	}
+	++n01;
+	if (n01 == 4) {
+		if (expSum != actSum) {
+			DBG_ERROR("Wrong sum, expected sum: " << std::hex << expSum << " - actual sum: " << actSum)
+		}
+		else {
+			DBG_INFO("Message complete");
+		}
+		toOutside();
+	}
+}
+
+void SMLParser::crc16(const byte ch)
+{
+	static uint16_t crc16_table[] = {
+			0xf78f, 0xe70e, 0xd68d, 0xc60c,
+			0xb58b, 0xa50a, 0x9489, 0x8408,
+			0x7387, 0x6306, 0x5285, 0x4204,
+			0x3183, 0x2102, 0x1081, 0x0000
+	};
+
+	actSum = ( 0xf000 | (actSum >> 4) ) ^ crc16_table[(actSum & 0xf) ^ (ch & 0xf)];
+	actSum = ( 0xf000 | (actSum >> 4) ) ^ crc16_table[(actSum & 0xf) ^ (ch >> 4)];
 }
 
 void SMLParser::parse(byte ch)
 {
-//	DBG(parser.level) << "Parsing level " << int{level} << ": " << std::hex << int{ch} << "\n";
-	if (!isInside) {
-		outside(ch);
-	}
-	else {
-		inside(ch);
+	DBG_TRACE("Parsing level " << getLevel() << ": ch = 0x" << std::hex << int{ch})
+	switch (state) {
+	case State::outside:	outside(ch);	break;
+	case State::inside:		inside(ch);		break;
+	case State::sum:		sum(ch);		break;
 	}
 }
 
--- parser/parser.hpp
+++ parser/parser.hpp
@@ -6,14 +6,20 @@
 #define byte uint8_t
 #endif
 
+#define SML_STACK_SIZE 10
+#define SML_STRING_MAX_LEN 64
+
 class SMLParser {
 public:
 	void reset();
 	void parse(byte ch);
 
 private:
-	bool isInside = false;
-	byte level = 0;  //!< Stack level
+	enum class State : byte {
+	      outside = 0
+		, inside = 1
+		, sum = 2
+	} state = State::outside;
 	struct StackEntry {
 		enum class Type : byte {
 			  tbd = 255
@@ -22,35 +28,51 @@ private:
 			, integer = 5
 			, uinteger = 6
 			, array = 7
-		} type = Type::tbd;
-		uint16_t length = 0;
-		uint16_t current = 0;
-		bool lengthFollows = false;
+		} type;
+		uint16_t length;
+		uint16_t current;
 		void init();
-		void tbd(SMLParser& parser, const byte ch);  //!< Parse type byte to determine entry type
-		void array(SMLParser& parser, const byte ch);  //!< Parse array
-		void boolean(SMLParser& parser, const byte ch);  //!< Parse array
-		void integer(SMLParser& parser, const byte ch, const bool hasSign);  //!< Parse integer
-		void string(SMLParser& parser, const byte ch);  //!< Parse integer
-		void done(SMLParser& parser);
-		union {
-			uint64_t	uinteger;
-			int64_t		integer;
-			bool		boolean;
-			byte		string[sizeof(uint64_t)];
-		} value = { 0 };
-		union {
-			byte* next;
-			byte protoBytes;
-		};
 	};
+	union {
+		uint64_t	uinteger;
+		int64_t		integer;
+		bool		boolean;
+		byte		string[SML_STRING_MAX_LEN];
+	} value;
+	union {
+		byte* next;
+		byte protoBytes;
+	};
+	bool lengthFollows = false;
 	using Type = StackEntry::Type;
-	std::array<StackEntry, 6> stack;
+	StackEntry stack[SML_STACK_SIZE];
+	StackEntry* level = stack;  //!< Current stack level
 	byte n1b = 0;
 	byte n01 = 0;
 
+	uint16_t actSum, expSum;
+
+	void toOutside();  //! Change to and initialize outside state
 	void outside(const byte ch); //!< Look for start sequence
+	void toInside();  //! Change to and initialize inside state
 	void inside(const byte ch); //!< Parse SML structure
+	void toSum();  //! Change to and initialize sum state
+	void sum(const byte ch); //!< Gather and verify sum
+
+	bool tbd(const byte ch);  //!< Parse type byte to determine entry type
+	bool array(const byte ch);  //!< Parse array
+	bool boolean(const byte ch);  //!< Parse array
+	bool integer(const byte ch, const bool hasSign);  //!< Parse integer
+	bool string(const byte ch);  //!< Parse integer
+	void done();
+
+	void crc16(const byte ch);  // Calculate crc16 on the fly
+	void x25crc16(const byte ch);  // Calculate crc16 on the fly
+
 	void enter();  //!< Enter next stack level
 	void leave();  //!< Leave stack level
+
+#if defined(DBG_LEVEL) && DBG_LEVEL > 0
+	int getLevel() const;
+#endif
 };
